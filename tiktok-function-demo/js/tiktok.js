34d91823aa1d2dd7a6c7a1cf3ccd78b7367a6ec9
jgoetzmann <jgoetzmann@eastsideprep.org>
2022-01-18 18:18:17 -0800

    Fixed bug and added isfav/isfriend

--- tiktok-function-demo/js/tiktok.js
+++ tiktok-function-demo/js/tiktok.js
@@ -34,18 +34,34 @@ function rankingAlgorithm(numLikes, numComments, numShares, comment, ifFriend, i
     }
 }
 
-// favthing prompts
-let favThing = prompt("What is you favorite thing?");
-// num prompts
-let numLikes = Number(prompt("How many likes did this tiktok get?"));
-let numShares = Number(prompt("How many shares did this tiktok get?"));
-let numComments = Number(prompt("How many comments did this tiktok get?"));
-// comment prompt
-let comment = prompt("What is the most recent comment on this tiktok?");
-// friend/fav prompt
-let ifFriend = prompt("Is this a tiktok from your friend? (y/n)");
-let ifFavorited = prompt("Have you favorited this tiktok? (y/n)");
+function rankingCall() {
+    // favthing prompts
+    let favThing = prompt("What is you favorite thing?");
+    // num prompts
+    let numLikes = Number(prompt("How many likes did this tiktok get?"));
+    let numShares = Number(prompt("How many shares did this tiktok get?"));
+    let numComments = Number(prompt("How many comments did this tiktok get?"));
+    // comment prompt
+    let comment = prompt("What is the most recent comment on this tiktok?");
+    // friend/fav prompt
+    let ifFriend = prompt("Is this a tiktok from your friend? (y/n)");
+    let ifFavorited = prompt("Have you favorited this tiktok? (y/n)");
 
-// function call and return
-let rankingReturn = rankingAlgorithm(numLikes, numComments, numShares, comment, ifFriend, ifFavorited, favThing);
-alert("This tiktok is #" + rankingReturn);
+    // function call and return
+    let rankingReturn = rankingAlgorithm(numLikes, numComments, numShares, comment, ifFriend, ifFavorited, favThing);
+    alert("This tiktok is #" + rankingReturn);
+}
+
+function showUI() {
+    let likeCount = Number(document.getElementById("like-textbox").value);
+    let shareCount = Number(document.getElementById("share-textbox").value);
+    let commentCount = Number(document.getElementById("comment-textbox").value);
+    let favThing = document.getElementById("fav-thing-textbox").value;
+    let topComment = document.getElementById("top-comment-textbox").value;
+
+    let rankResult = rankingAlgorithm(likeCount, commentCount, shareCount, topComment, "n", "n", favThing);
+
+    document.getElementById("result-paragraph").innerHTML = "Tiktok Rank: " + rankResult;
+}
+
+showUI();
