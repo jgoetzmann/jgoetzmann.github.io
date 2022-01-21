28c2f49179c544016d00fa5b3d95034aa87c34ba
jgoetzmann <jgoetzmann@eastsideprep.org>
2022-01-21 09:43:15 -0800

    Made a table :D

--- p1/table-project/js/table.js
+++ p1/table-project/js/table.js
@@ -1,1 +1,7 @@
 console.log("Hello World")
+
+const bp = [100, 200, 120, 800];
+const moves = ["Jay beam", "Rishay Punch", "Ethan Slam", "Soren Smack"];
+
+console.log(bp[0]);
+console.log(moves[0]);
