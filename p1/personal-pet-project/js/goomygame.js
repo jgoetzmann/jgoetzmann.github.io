0805ba1ab76b0e48b7b946c202e30a3a7ec39b2f
jgoetzmann <jgoetzmann@eastsideprep.org>
2022-01-25 22:24:31 -0800

    UI Framework

--- p1/personal-pet-project/js/goomygame.js
+++ p1/personal-pet-project/js/goomygame.js
@@ -1,1 +1,23 @@
-console.log("Hello Goomyakalhdajkhdajkd")
+console.log("Hello World")
+
+const bp = [100, 200, 120, 800];
+const moves = ["Jay beam", "Rishay Punch", "Ethan Slam", "Soren Smack"];
+
+console.log(bp[0]);
+console.log(moves[0]);
+
+let happiness = 100;
+let hunger = 100;
+let health = 100;
+
+function amuseGoomy() {
+    console.log("Amuse Trigger");
+}
+
+function feedGoomy() {
+    console.log("Feed Trigger");
+}
+
+function healGoomy() {
+    console.log("Heal Trigger");
+}
