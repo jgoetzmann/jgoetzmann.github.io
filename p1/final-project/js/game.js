3bf293630c2600953bcd38773cd94d49418ae896
jgoetzmann <jgoetzmann@eastsideprep.org>
2022-02-16 20:56:45 -0800

    Skeleton

--- p1/final-project/js/game.js
+++ p1/final-project/js/game.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+// gets canvas element
+let map = document.getElementById("map");
+let ctx = map.getContext("2d");
+
+function clickTest(click) {
+    console.log(click)
+}
+
+// draw rectangle
+ctx.fillStyle = "#808080";
+ctx.fillRect(0, 230, 240, 20);
+ctx.fillRect(220, 130, 20, 120);
+ctx.fillRect(220, 130, 240, 20);
+ctx.fillRect(460, 130, 20, 220);
+ctx.fillRect(460, 330, 240, 20);
+ctx.fillRect(700, 230, 20, 120);
+ctx.fillRect(720, 230, 240, 20);
